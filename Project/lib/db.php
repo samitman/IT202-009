51336db3e20af183a19b1c7c7dfff5df423f73f1
samitman <si237@njit.edu>
2020-10-01 13:55:53 -0400

    loading correct db.php file

--- Project/lib/db.php
+++ Project/lib/db.php
@@ -11,7 +11,9 @@ function getDB(){
     if(!isset($db)) {
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
-            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
+            //it gets the absolute path to this file, then we append the relative url
+            // (so up a directory and inside lib)           
+            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
@@ -23,3 +25,5 @@ function getDB(){
         }
     }
     return $db;
+}
+?>
