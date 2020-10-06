1152102b03121e1f9fce77da4c8e88756e835896
samitman <si237@njit.edu>
2020-10-06 17:07:12 -0400

    updating files for project prep

--- Project/lib/db.php
+++ Project/lib/db.php
@@ -11,17 +11,17 @@ function getDB(){
     if(!isset($db)) {
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
-            //it gets the absolute path to this file, then we append the relative url
-            // (so up a directory and inside lib)           
-            require_once(__DIR__."/../lib/config.php"); //pull in our credentials
+            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
+            require_once(__DIR__."/config.php"); //pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
             //if no error occurs we're connected
             $db = new PDO($connection_string, $dbuser, $dbpass);
-	}
-         catch(Exception $e){
+        }
+    catch(Exception $e){
             var_export($e);
+            $db = null;
         }
     }
     return $db;
