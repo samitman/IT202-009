73ba2d0747d5134ee20edefee23de90b2fe5fa5c
si237 <si237@njit.edu>
2020-11-10 01:42:53 +0000

    loading Admin 2 homework: Shopping cart

---
+++ project/test_edit_cart.php
@@ -0,0 +1,80 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+//we'll put this at the top so both php block have access to it
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+
+//fetching
+$result = [];
+if (isset($id)) {
+    $id = $_GET["id"];
+    $db = getDB();
+    $stmt = $db->prepare("SELECT * FROM Cart where id = :id");
+    $r = $stmt->execute([":id" => $id]);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+$db = getDB();
+$stmt = $db->prepare("SELECT id,name,price from Products LIMIT 10");
+$r = $stmt->execute();
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+    <h3>Edit Cart</h3>
+    <form method="POST">
+       <label>Select a Product</label>
+        <select name="product_id">
+            <option value="-1">None</option>
+            <?php foreach ($products as $product): ?>
+                <option value="<?php safer_echo($product["id"]); ?>"> <?php safer_echo($product["name"]." $".$product["price"]);?> </option>
+                <?php endforeach; ?>
+        </select>
+        <label>Quantity</label>
+        <input name="quantity" type="number"/>
+        <input type="submit" name="save" value="Update"/>
+    </form>
+<?php
+//saving
+if (isset($_POST["save"])) {
+    if(isset($_POST["product_id"])){
+                $product_id = $_POST["product_id"];
+                $db = getDB();
+                $stmt = $db->prepare("SELECT id,price from Products WHERE id=:id");
+                $r = $stmt->execute([":id" => $product_id]);
+                $productSelection = $stmt->fetch(PDO::FETCH_ASSOC);
+         }
+    $product_id = $_POST["product_id"];
+    $quantity = $_POST["quantity"];
+    $price = $productSelection["price"];
+    $user = get_user_id();
+    $db = getDB();
+    if (isset($id)) {
+        $stmt = $db->prepare("UPDATE Cart set product_id=:product_id, quantity=:quantity, price=:price where id=:id");
+        $r = $stmt->execute([
+            ":product_id" => $product_id,
+            ":quantity" => $quantity,
+            ":price" => $price,
+            ":id" => $id
+        ]);
+        if ($r) {
+            flash("Updated successfully with id: " . $id);
+        }
+        else {
+            $e = $stmt->errorInfo();
+            flash("Error updating: " . var_export($e, true));
+        }
+    }
+    else {
+        flash("ID isn't set, we need an ID in order to update");
+        }
+}
+?>
+
+<?php require(__DIR__ . "/partials/flash.php");
---
+++ project/test_list_carts.php
@@ -0,0 +1,56 @@
+
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+<?php
+$query = "";
+$results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT cart.id,cart.product_id,cart.price,cart.quantity,product.name as product, Users.username from Cart as cart JOIN Users on cart.user_id = Users.id LEFT JOIN Products as product on cart.product_id = product.id WHERE cart.id like :q LIMIT 10");
+    $r = $stmt->execute([":q" => "%$query%"]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results " . var_export($stmt->errorInfo(), true));
+    }
+}
+?>
+<h3>List Carts</h3>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $r): ?>
+                <div class="list-group-item">
+                  <div>
+			<div>Username: <?php safer_echo($r["username"]); ?></div>
+		  </div>
+
+		  <div>
+			<div> Cart ID: <?php safer_echo($r["id"]); ?></div>
+		  </div>
+                    <div>
+                        <a type="button" href="test_edit_cart.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_cart.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+		    <div> <br> </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
