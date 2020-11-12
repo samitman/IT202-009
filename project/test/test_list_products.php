3cba2d905b8b72c51def2a17d8ff2d7a24e06c2f
samitman <si237@njit.edu>
2020-11-12 17:12:11 -0500

    Added category to edit and search pages for products

--- project/test/test_list_products.php
+++ project/test/test_list_products.php
@@ -14,7 +14,7 @@ if (isset($_POST["query"])) {
 }
 if (isset($_POST["search"]) && !empty($query)) {
     $db = getDB();
-    $stmt = $db->prepare("SELECT id,name,price,quantity, user_id from Products WHERE name like :q LIMIT 10");
+    $stmt = $db->prepare("SELECT id,name,price,quantity, user_id from Products WHERE name like :q OR category like :q LIMIT 10");
     $r = $stmt->execute([":q" => "%$query%"]);
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
