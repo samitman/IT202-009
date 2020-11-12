6582b9ba829bac138534791010cd9c909442728c
samitman <si237@njit.edu>
2020-11-11 20:14:24 -0500

    removing ksort funct

--- project/catalog.php
+++ project/catalog.php
@@ -6,7 +6,7 @@
     $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id, Users.username FROM Products JOIN Users on Products.user_id = Users.id");
     $r = $stmt->execute([]);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    $results = ksort($results);
+    echo var_export($results);
 ?>
 <div class="results">
         <div class="list-group">
