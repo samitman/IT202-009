596e0f32761ae0d0fba514d3dfe8f1fbed33f18e
samitman <si237@njit.edu>
2020-11-12 16:15:11 -0500

    updated query parameters in catalog

--- project/catalog.php
+++ project/catalog.php
@@ -2,15 +2,21 @@
 <p>Product Catalog</p>
 
 <?php
+if (!has_role("Admin")) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE Products.visibility != 0");
+    $r = $stmt->execute([]);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} elseif (has_role("Admin")) {
     $db = getDB();
     $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility Users.username FROM Products JOIN Users on Products.user_id = Users.id");
     $r = $stmt->execute([]);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 <div class="results">
         <div class="list-group">
             <?php foreach ($results as $product): ?>
-                <?php if (!has_role("Admin") && $product["visibility"]=="0"){continue;}?>
                 <div class="list-group-item">
                     <div>
                         <div>Name: <?php safer_echo($product["name"]); ?></div>
