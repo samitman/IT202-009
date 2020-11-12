542f7e37197e9d84afbca1e5b159cf2766d20fce
samitman <si237@njit.edu>
2020-11-12 18:49:53 -0500

    back to original search page

--- project/search.php
+++ project/search.php
@@ -1,16 +1,17 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
-
 <p>Search Products</p>
 
 <?php
 $query = "";
 $results = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
 if (isset($_POST["search"]) && !empty($query)) {
-    $filter = $_POST["filter"];
     if (!has_role("Admin")) {
         $db = getDB();
-        $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility,category, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE name like :q AND Products.visibility!=0 ORDER BY name LIMIT 10");
+        $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE name like :q AND Products.visibility!=0 LIMIT 10 ORDER BY name");
         $r = $stmt->execute([":q" => "%$query%"]);
         if ($r) {
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -19,7 +20,7 @@ if (isset($_POST["search"]) && !empty($query)) {
         }
     } elseif (has_role("Admin")) {
         $db = getDB();
-        $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility,category, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE name like :q ORDER BY name LIMIT 10");
+        $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE name like :q LIMIT 10 ORDER BY name");
         $r = $stmt->execute([":q" => "%$query%"]);
         if ($r) {
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -31,12 +32,6 @@ if (isset($_POST["search"]) && !empty($query)) {
 ?>
 <form method="POST">
     <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
-    <br><br>
-    <label for="filter">Filter:</label>
-    <select name="filter" id="filter">
-        <option value="name">Name</option>
-        <option value="category">Category</option>
-    </select>
     <input type="submit" value="Search" name="search"/>
 </form>
 <div class="results">
@@ -49,9 +44,6 @@ if (isset($_POST["search"]) && !empty($query)) {
                     </div>
                     <div>
                         <div>Price: <?php safer_echo($r["price"]); ?></div>
-                    </div>
-                    <div>
-                        <div>Category: <?php safer_echo($r["category"]); ?></div>
                     </div>
                     <div>
                         <div>Units Available: <?php safer_echo($r["quantity"]); ?></div>
@@ -72,3 +64,4 @@ if (isset($_POST["search"]) && !empty($query)) {
         <p>No results</p>
     <?php endif; ?>
 </div>
+
