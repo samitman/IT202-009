a15129c0ecd3190409ba80af43c9390c6dfda22a
samitman <si237@njit.edu>
2020-11-11 19:01:29 -0500

    created project catalog page

---
+++ project/catalog.php
@@ -0,0 +1,46 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<p>Product Catalog</p>
+
+<?php
+    $db = getDB();
+    $stmt = $db->prepare("SELECT id,name,price,quantity,user_id, Users.username FROM Products JOIN Users on Products.user_id = Users.id");
+    $r = $stmt->execute([]);
+    if ($r) {
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
+?>
+<div class="results">
+    <?php if (count($results) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($results as $product): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Name:</div>
+                        <div><?php safer_echo($product["name"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Price:</div>
+                        <div><?php safer_echo($product["price"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Units Available:</div>
+                        <div><?php safer_echo($product["quantity"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Seller ID:</div>
+                        <div><?php safer_echo($product["username"]); ?></div>
+                    </div>
+                    <div>
+                        <a type="button" href="test_edit_product.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="test_view_product.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
--- project/partials/nav.php
+++ project/partials/nav.php
@@ -19,6 +19,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="<?php echo getURL("test/test_list_carts.php"); ?>">View Carts</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
+            <li><a href="<?php echo getURL("catalog.php"); ?>">Catalog</a></li>
             <li><a href="<?php echo getURL("profile.php"); ?>">Profile</a></li>
             <li><a href="<?php echo getURL("logout.php"); ?>">Logout</a></li>
         <?php endif; ?>
