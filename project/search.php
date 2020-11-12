bcee532db89fa0d6b07fdef3bef0ddac89e322bf
samitman <si237@njit.edu>
2020-11-12 17:31:23 -0500

    trying to find error in search page

--- project/search.php
+++ project/search.php
@@ -30,6 +30,9 @@ if (isset($_POST["search"]) && !empty($query)) {
     }
 }
 ?>
+
+<?php var_export($results);?>
+
 <form method="POST">
     <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
     <input type="submit" value="Search" name="search"/>
