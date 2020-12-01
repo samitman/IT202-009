b7694f4e07c1d64e440cea41bf5e6b9b653f472a
samitman <si237@njit.edu>
2020-12-01 18:52:57 -0500

    updated home page

--- project/home.php
+++ project/home.php
@@ -4,7 +4,14 @@
 $email = "";
 if (isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])) {
     $email = $_SESSION["user"]["email"];
-}
+    echo "Welcome,".$email;
+} elseif (!is_logged_in()) {
+            flash("Welcome, please login!");
+        }
+
+
 ?>
-    <p>Welcome, <?php echo $email; ?></p>
+<img src="<?php getURL("/static/css/protein1.jpg/")?>" width="500" height="500">
+<img src="<?php getURL("/static/css/protein2.jpg/")?>" width="500" height="500">
+<img src="<?php getURL("/static/css/protein3.jpg/")?>" width="500" height="500">
 <?php require(__DIR__."/partials/flash.php"); ?>
