caa7d8a0f7c71c36ede858f933e41e2a7cfa2386
samitman <si237@njit.edu>
2020-12-07 18:07:32 -0500

    disabling previous/next links based on current page

--- project/catalog.php
+++ project/catalog.php
@@ -67,15 +67,19 @@ if (!has_role("Admin")) {
     <div>
         <nav aria-label="Pages">
             <ul class="pagination">
+                <?php if(!(($page-1)<1)):?>
                 <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
                     <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
                 </li>
+                <?php endif; ?>
                 <?php for($i = 0; $i < $total_pages; $i++):?>
                     <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
                 <?php endfor; ?>
+                <?php if($page<$total_pages):?>
                 <li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
                     <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
                 </li>
+                <?php endif; ?>
             </ul>
         </nav>
     </div>
