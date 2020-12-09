26209c09ba0beaa0b5d2a638edc210a037b3844d
samitman <si237@njit.edu>
2020-12-09 12:00:27 -0500

    tried adding pagination to product search by name

--- project/search.php
+++ project/search.php
@@ -22,12 +22,38 @@ if (isset($_POST["search"]) && !empty($query) && isset($_POST["filter"])) {
                 break;
         }
 
+        //saving filter as a session variable
+        $_SESSION["filter"] = $safeFilter;
+        //getting pagination values
+        $page = 1;
+        $per_page = 5;
+        if(isset($_GET["page"])){
+            try {
+                $page = (int)$_GET["page"];
+            }
+            catch(Exception $e){
+
+            }
+        }
+        $db = getDB();
+        $stmt = $db->prepare("SELECT count(*) as total from Products");
+        $stmt->execute([]);
+        $productResult = $stmt->fetch(PDO::FETCH_ASSOC);
+        $total = 0;
+        if($productResult){
+            $total = (int)$productResult["total"];
+        }
+        $total_pages = ceil($total / $per_page);
+        $offset = ($page-1) * $per_page;
 
         if($safeFilter == "category" || $safeFilter == "name") {
             if (!has_role("Admin")) {
                 $db = getDB();
-                $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility,category, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE $safeFilter LIKE :q AND Products.visibility!=0 AND Products.quantity > 0 ORDER BY name LIMIT 10");
-                $r = $stmt->execute([":q" => "%$query%"]);
+                $stmt = $db->prepare("SELECT Products.id,name,quantity,price,user_id,visibility,category, Users.username FROM Products JOIN Users on Products.user_id = Users.id WHERE $safeFilter LIKE :q AND Products.visibility!=0 AND Products.quantity > 0 ORDER BY name LIMIT :offset, :count");
+                $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
+                $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
+                $stmt->bindValue(":q", $query);
+                $r = $stmt->execute();
                 if ($r) {
                     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                 } else {
@@ -111,4 +137,25 @@ if (isset($_POST["search"]) && !empty($query) && isset($_POST["filter"])) {
         <p>No results</p>
     <?php endif; ?>
 </div>
+    <div>
+        <nav aria-label="Pages">
+            <ul class="pagination">
+                <?php if(!(($page-1)<1)):?>
+                    <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+                        <a class="page-link" href="?page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+                    </li>
+                <?php endif; ?>
+                <?php for($i = 0; $i < $total_pages; $i++):?>
+                    <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+                <?php endfor; ?>
+                <?php if($page<$total_pages):?>
+                    <li class="page-item <?php echo ($page) >= $total_pages?"disabled":"";?>">
+                        <a class="page-link" href="?page=<?php echo $page+1;?>">Next</a>
+                    </li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </div>
+
+<?php require(__DIR__ . "/partials/flash.php");
 
