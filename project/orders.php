214da32fc3d1936110a6a77e1ba255bd97adbe43
samitman <si237@njit.edu>
2020-12-14 11:16:12 -0500

    checking if filter is set before getting pagination vars

--- project/orders.php
+++ project/orders.php
@@ -50,15 +50,17 @@ if(!has_role("Admin")) {
         $stmt->execute();
         $orderResult = $stmt->fetch(PDO::FETCH_ASSOC);
     }
-        if($filter=="category" && !empty($cat)){
+    if(isset($filter)) {
+        if ($filter == "category" && !empty($cat)) {
             $stmt = $db->prepare("SELECT count(*) as total from OrderItems JOIN Products on product_id=Products.id where Products.category=:cat");
-            $stmt->execute([":cat"=>$cat]);
+            $stmt->execute([":cat" => $cat]);
             $orderResult = $stmt->fetch(PDO::FETCH_ASSOC);
-        }elseif($filter=="date" && !empty($date1) && !empty($date2)){
+        } elseif ($filter == "date" && !empty($date1) && !empty($date2)) {
             $stmt = $db->prepare("SELECT count(*) as total from Orders WHERE created BETWEEN :date1 and :date2");
-            $stmt->execute([":date1"=>$date1,":date2"=>$date2]);
+            $stmt->execute([":date1" => $date1, ":date2" => $date2]);
             $orderResult = $stmt->fetch(PDO::FETCH_ASSOC);
         }
+    }
 }
 
 
